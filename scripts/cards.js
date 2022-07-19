735139111ba3ce9c0dda019a0fc82eec6e7c9fed
Daria Doronina <Daria-VladimS@yandex.ru>
2022-07-19 20:54:07 +0300

    feat: full project num five

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,53 +1,26 @@
-/*const initialCards = [
-    {
-      name: 'Иваново',
-      link: 'https://disk.yandex.ru/i/VRTkh6Lky_9gKA'
-    },
-    {
-      name: 'Сочи',
-      link: 'https://disk.yandex.ru/i/qFQFt9gyfzDZlQ'
-    },
-    {
-      name: 'Санкт-Петербург',
-      link: 'https://disk.yandex.ru/i/x0R0mkCAhe5Lxw'
-    },
-    {
-      name: 'Красноярский край',
-      link: 'https://disk.yandex.ru/i/Oa9dEoz5xL6o6w'
-    },
-    {
-      name: 'Приморский край',
-      link: 'https://disk.yandex.ru/i/36G4UsUDnCa_Og'
-    },
-    {
-      name: 'Первушино',
-      link: 'https://disk.yandex.ru/i/RvJ7WhApsiYeNg'
-    }
-];*/
-
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
     name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    link: 'https://goo.su/Y4ivO'
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Сочи',
+    link: 'https://goo.su/G8wxjD'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Санкт-Петербург',
+    link: 'https://goo.su/gzw1lKp'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Красноярский край',
+    link: 'https://goo.su/xgDQ'
+  },
+  {
+    name: 'Приморский край',
+    link: 'https://goo.su/jNszhSk'
+  },
+  {
+    name: 'Первушино',
+    link: 'https://goo.su/SjIpXZJ'
   }
 ];
