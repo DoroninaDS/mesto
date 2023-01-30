dacc8ef6f71912490eb6a1a281b6ac7d7d113ffd
Daria Doronina <Daria-VladimS@yandex.ru>
2023-01-30 23:29:46 +0300

    fix: исправить замечания ревьюера pt2 project 9

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup{
     constructor(popupSelector) {
         super(popupSelector);
-        this._imageFull = this._popupElement.querySelector('.popup__figure_image');
-        this._titleFull = this._popupElement.querySelector('.popup__figure_title');
+        this._imageFull = this._popupElement.querySelector('.popup__figure-image');
+        this._titleFull = this._popupElement.querySelector('.popup__figure-title');
     }
 
     open (name, link) {
